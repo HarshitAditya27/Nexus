4972d4410f036d7d88f9d4c6916660619c074992
Harshit Aditya <harshitaditya27@gmail.com>
2024-08-25 20:48:10 +0530

    Added organizations in dashboard header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
 import Logo from "./Logo";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
 
 function Header() {
+  const { orgId } = useAuth();
   return (
     <div className="flex justify-between items-center p-3 shadow-sm">
       <Logo />
+      <OrganizationSwitcher />
       <UserButton />
     </div>
   );
