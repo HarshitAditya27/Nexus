7f194d0c14739288b11fb1a9f05e4986c08e47dc
Harshit Aditya <harshitaditya27@gmail.com>
2024-08-30 23:05:28 +0530

    Added listing of workspaces functionality in dashboard

---
+++ app/_components/WorkspaceItemList.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import React from "react";
+
+function WorkspaceItemList({ workspaceList }) {
+  const router = useRouter();
+  const onClickWorkspaceItem = (workspaceId) => {
+    router.push("/workspace" + workspaceId);
+  };
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+      {workspaceList &&
+        workspaceList.map((workspace, index) => (
+          <div
+            key={index}
+            className="border shadow-xl rounded-xl hover:scale-105 transition-all cursor-pointer"
+            onClick={() => onClickWorkspaceItem(workspace.id)}
+          >
+            <Image
+              src={workspace?.coverImage}
+              width={400}
+              height={200}
+              alt="Cover"
+              className="h-[150px] object-cover rounded-t-xl"
+            />
+            <div className="p-4 rounded-b-xl">
+              <h2 className="flex gap-2">
+                {" "}
+                {workspace?.emoji} {workspace.workspaceName}
+              </h2>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default WorkspaceItemList;
--- app/_components/WorkspaceList.jsx
+++ app/_components/WorkspaceList.jsx
@@ -1,15 +1,41 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { AlignLeft, LayoutGrid } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import WorkspaceItemList from "./WorkspaceItemList";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/config/firebaseConfig";
 
 function WorkspaceList() {
   const { user } = useUser();
+  const { orgId } = useAuth();
   const [workspaceList, setWorkspaceList] = useState([]);
+
+  useEffect(() => {
+    user && getWorkspaceList();
+  }, [orgId, user]);
+
+  const getWorkspaceList = async () => {
+    const q = query(
+      collection(db, "workspace"),
+      where(
+        "orgId",
+        "==",
+        orgId ? orgId : user?.primaryEmailAddress?.emailAddress
+      )
+    );
+    const querySnapshots = await getDocs(q);
+
+    querySnapshots.forEach((doc) => {
+      console.log(doc.data());
+      setWorkspaceList((prev) => [...prev, doc.data()]);
+    });
+  };
+
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
       <div className="flex justify-between">
@@ -44,7 +70,10 @@ function WorkspaceList() {
           </Link>
         </div>
       ) : (
-        <div> Workspace List</div>
+        <div>
+          {" "}
+          <WorkspaceItemList workspaceList={workspaceList} />{" "}
+        </div>
       )}
     </div>
   );
